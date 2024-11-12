3d86fb503c3b83fa90f0cf4312dace3d112c4fbe
Helber-source <luizhelber5@gmail.com>
2024-11-12 12:14:18 -0300

    Add files via upload

---
+++ Solução.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ativar_orbe(int codigo_secreto) {
+    
+    int numero_mistico = ((42 * 3) - (6 * 7)) + (12 * 3); 
+
+
+    if (codigo_secreto == numero_mistico) {
+        printf("O Orbe da Sabedoria brilha intensamente... Parabéns, você revelou seu segredo!\n");
+        return 1;
+    } else {
+        printf("O Orbe permanece inerte. Parece que este não é o código certo...\n");
+        return 0;
+    }
+}
+
+int main() {
+    int codigo_secreto;
+    
+    
+    printf("Digite o código secreto para ativar o Orbe da Sabedoria: ");
+    scanf("%d", &codigo_secreto);
+
+    
+    ativar_orbe(codigo_secreto);
+
+    return 0;
+}
+
